517f4afcd39d28afc0e8055c605e0c2deebda3f3
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-10-17 16:35:48 +0300

    Using env var for firebase secret because google didn't like the other way

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => {
                 provider: "strapi-provider-firebase-storage",
                 providerOptions: {
                     // @ts-ignore
-                    serviceAccount: require("./ecommerce-de40d-firebase-adminsdk-rcpwk-f3ba1289cc.json"),
+                    serviceAccount: env("FIREBASE_PRIVATE_KEY"),
                     // Custom bucket name
                     bucket: "ecommerce-de40d.appspot.com",
                     sortInStorage: true, // true | false
